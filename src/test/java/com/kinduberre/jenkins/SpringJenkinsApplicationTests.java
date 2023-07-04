543a5ab9670e90dfbe23a6d1736886495d2698c3
dank3m <dankimist@gmail.com>
2023-07-04 09:23:42 +0300

    first commit

---
+++ src/test/java/com/kinduberre/jenkins/SpringJenkinsApplicationTests.java
@@ -0,0 +1,22 @@
+package com.kinduberre.jenkins;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest
+class SpringJenkinsApplicationTests {
+
+	public static Logger logger = LoggerFactory.getLogger(SpringJenkinsApplicationTests.class);
+	
+	@Test
+	void contextLoads() {
+		logger.info("Test case executing ...");
+		assertEquals(true, true);
+	}
+
+}
